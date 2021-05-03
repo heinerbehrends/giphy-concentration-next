9f7b46905a88f7f7bb9ae98a6ad668b8887d6f56
heinerbehrends <heinerbehrends@hotmail.com>
2021-05-03 17:03:01 +0200

    Added encodeURIComponent to redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    router.push(`${input}`);
+    router.push(encodeURIComponent(input));
   }
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setInput(event.target.value);

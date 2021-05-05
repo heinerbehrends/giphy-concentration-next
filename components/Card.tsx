1ee765d04cc4a2a67e7fd0947c611e7105b445e7
heinerbehrends <heinerbehrends@hotmail.com>
2021-05-05 15:57:36 +0200

    removed unused import

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { CardT } from '../logic/logic';
 import { CardScene, CardContainer, CardFront, CardBack } from './CardStyles';
 

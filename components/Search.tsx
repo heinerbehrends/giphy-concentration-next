e145153cc9766169f06a16a5bf57c7142b17aff0
heinerbehrends <heinerbehrends@hotmail.com>
2021-09-16 16:01:06 +0200

    Added the Replay button

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,25 +1,36 @@
 import React, { ChangeEventHandler, FormEventHandler } from 'react';
+import { useRouter } from 'next/router';
 import {
   SearchSection,
   Heading,
   Form,
   TextInput,
   Button,
+  ReplayButton,
 } from './SearchStyles';
 
 type SearchProps = {
   handleChange: ChangeEventHandler<HTMLInputElement>;
   handleSubmit: FormEventHandler<HTMLFormElement>;
   value: string;
+  nrOfCardsTurned: number;
 };
 
 export default function Search({
   handleSubmit,
   handleChange,
   value,
+  nrOfCardsTurned,
 }: SearchProps) {
+  const router = useRouter();
   return (
     <SearchSection>
+      {nrOfCardsTurned > 0 ? (
+        <ReplayButton as="button" onClick={() => router.back()}>
+          Play again!
+        </ReplayButton>
+      ) : null}
+
       <Heading>Search Giphy to start a game</Heading>
       <Form onSubmit={handleSubmit}>
         <TextInput

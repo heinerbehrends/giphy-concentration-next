641b49b777ef5b2a3c7a95f0d31e658dd37a2277
heinerbehrends <heinerbehrends@hotmail.com>
2021-09-16 22:03:19 +0200

    Removed unnecessary props

--- components/Confetti.tsx
+++ components/Confetti.tsx
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactConfetti from 'react-confetti';
 
 type ConfettiProps = {
-  setShowConfetti: React.Dispatch<React.SetStateAction<number>>;
-  showConfetti: number;
   shouldRecycle: boolean;
 };
-export default function Confetti({
-  setShowConfetti,
-  showConfetti,
-  shouldRecycle = false,
-}: ConfettiProps) {
-  return (
-    <ReactConfetti
-      recycle={shouldRecycle}
-      onConfettiComplete={() => setShowConfetti(showConfetti - 1)}
-    />
-  );
+export default function Confetti({ shouldRecycle }: ConfettiProps) {
+  return <ReactConfetti recycle={shouldRecycle} />;
 }

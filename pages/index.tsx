5cf17a26866d958b01d8b5b742108f8043d58e91
heinerbehrends <heinerbehrends@hotmail.com>
2021-09-16 16:05:48 +0200

    Added nrOfCardsTurned prop, pass it to Search

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import Search from '../components/Search';
 import { useSearch } from '../logic/useSearch';
 
-export default function Home() {
+type HomeProps = {
+  nrOfCardsTurned: number;
+};
+
+export default function Home({ nrOfCardsTurned }: HomeProps) {
   const { input, handleChange, handleSubmit } = useSearch();
   return (
-    <Search
-      handleChange={handleChange}
-      handleSubmit={handleSubmit}
-      value={input}
-    />
+    <>
+      <Search
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        value={input}
+        nrOfCardsTurned={nrOfCardsTurned}
+      />
+    </>
   );
 }
